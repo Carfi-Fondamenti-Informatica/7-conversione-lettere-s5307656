5631276e884448d925c9704d9c7d118e3f0288fc
s5307656 <100590984+s5307656@users.noreply.github.com>
2022-05-03 18:07:17 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
-
-int main(){
-  return 0;
-}
+#include "lib.h"
+using namespace std;
+    int main(){
+        char c;
+        cin >>c;
+        if (carattere(c)){
+            if (c>=97 && c<=122){
+                c= c-32;
+                } else {
+                c=c + 32;}
+            cout << (char)c;
+        }
+        else
+            cout << "errore" << endl;
+        return 0;
+    }
