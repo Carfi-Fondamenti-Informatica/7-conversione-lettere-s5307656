df9a27e3ec38eb58cfbf18871878692ac34cf29b
s5307656 <100590984+s5307656@users.noreply.github.com>
2022-05-03 18:11:16 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,7 +1,7 @@
 #include "lib.h"
 bool carattere (int c=0){
     if ((c>=97 && c<=122) || (c>=65 && c<=90)){
-            return true;
+            return true ;
     } else {
         return false;
     }
